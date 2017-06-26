86b9e68d95aa916120c5e81c538a8d0743d7879c
Rahul Tapali <rahul.tapali100@gmail.com>
2017-06-26 13:34:19 +0530

    First hello message from server

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import (
+        "net/http"
+        "fmt"
+        "github.com/gorilla/mux"
+)
 
-func main(){
-        
+func main() {
+        router := mux.NewRouter()
+        router.HandleFunc("/hello/{name}", handleSayHello).Methods("GET")
+        http.ListenAndServe(":8080", router)
+}
+
+func handleSayHello(rw http.ResponseWriter, req *http.Request) {
+        vars := mux.Vars(req)
+        msg := fmt.Sprint("Hello ", vars["name"])
+        fmt.Fprint(rw, msg)
 }
